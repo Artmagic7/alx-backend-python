df36759cfc5b2cc07464e714aa6c710475660870
Arthur Owens <arth.owens@gmail.com>
2024-04-18 22:30:02 +0000

    Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Task 2 Module.
+'''
+
+
+import math
+
+
+def floor(a: float) -> int:
+    '''Calculates the floor of a floating-point number.
+    '''
+    return math.floor(a)
